946a4df942232b4332f2531a930d273fe3283bc3
1-02 <1-02@DESKTOP-QLE8MTH>
2020-12-15 10:05:42 +0900

    d

--- gimsehyeon.yoyangscombine/src/main/java/gimsehyeon/yoyangscombine/web/report/ReportController.java
+++ gimsehyeon.yoyangscombine/src/main/java/gimsehyeon/yoyangscombine/web/report/ReportController.java
@@ -1,5 +1,6 @@
 package gimsehyeon.yoyangscombine.web.report;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +10,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import gimsehyeon.yoyangscombine.service.communication.pager.Pager;
+import gimsehyeon.yoyangscombine.service.report.ReportService;
 
 @Controller
 @SessionAttributes("report")
 public class ReportController {
+	@Autowired private ReportService reportService;	
 	
 	@RequestMapping("/report")
-	//public String allReport(Model model,int reportNum, @RequestParam(defaultValue="1") int curPage) throws Exception{
-	//	int count = reportService.calcReport(reportNum);
-	//	Pager pager = new Pager(count, curPage);
-	//	int start = pager.getPageBegin();
-	//	int end = pager.getPageEnd();	
-	//}
+	public String allReport(Model model,int reportNum, @RequestParam(defaultValue="1") int curPage) throws Exception{
+		int count = reportService.calcReport(reportNum);
+		Pager pager = new Pager(count, curPage);
+		int start = pager.getPageBegin();
+		int end = pager.getPageEnd();
+		
+
+		return "report/report";
+	}
 
 	@GetMapping("/report/report")
 	public String reportBoardin() {
